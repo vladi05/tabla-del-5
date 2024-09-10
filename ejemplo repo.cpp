062bbe2f521b330ff3e3b9e8f285348516c20f3b
betun plassy <betunplassy@gmail.com>
2024-09-10 15:19:52 -0300

    primer commit , crea tabla de multiplicar

---
+++ ejemplo repo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+int numero = 5;
+for (int i = 1; i <= 10; i++){
+
+    cout << "el numero " << " x " << i << " = " << numero * i << endl;
+
+}
+
+
+
+return 0;
+}
